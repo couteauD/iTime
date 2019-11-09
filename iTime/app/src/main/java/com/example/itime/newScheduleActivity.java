7ad94fa835accef30cb882cfbd1d7ba8010b2dca
couteauD <598731667@qq.com>
2019-11-09 23:49:45 +0800

    在newScheduleActicity中添加日期控件和时间控件，点击新建日程界面的日期可设置日期和时间

--- iTime/app/src/main/java/com/example/itime/newScheduleActivity.java
+++ iTime/app/src/main/java/com/example/itime/newScheduleActivity.java
@@ -1,13 +1,32 @@
 package com.example.itime;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class newScheduleActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.Calendar;
+
+public class newScheduleActivity extends AppCompatActivity implements View.OnClickListener,DatePicker.OnDateChangedListener,TimePicker.OnTimeChangedListener{
+
+    private Context context;
+    private RelativeLayout relativeLayoutDate;
+    private TextView dateInstuction,timeInstruction;
+    private int year, month, day, hour, minute;
+    //在TextView上显示的字符
+    private StringBuffer date, time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +37,120 @@ public class newScheduleActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        context=this;
+        date=new StringBuffer();
+        time=new StringBuffer();
+        initView();
+        initDateTime();
+    }
+
+    /**
+     * 初始化日期控件
+     */
+    private void initView() {
+        relativeLayoutDate = (RelativeLayout) findViewById(R.id.relativelayout_date);
+        dateInstuction=(TextView)findViewById(R.id.text_view_dateInstruction);
+        timeInstruction=(TextView)findViewById(R.id.text_view_timeInstruction);
+        relativeLayoutDate.setOnClickListener(this);
+    }
+
+    /**
+     * 获取当前的日期和时间
+     */
+    private void initDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR);
+        minute = calendar.get(Calendar.MINUTE);
+    }
+
+    @Override
+    public void onClick(View v){
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(context);
+        builder2.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (time.length() > 0) { //清除上次记录的日期
+                    time.delete(0, time.length());
+                }
+                timeInstruction.setText(time.append(String.valueOf(hour)).append("时").append(String.valueOf(minute)).append("分"));
+                dialog.dismiss();
+            }
+        });
+        builder2.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog2 = builder2.create();
+        View dialogView2 = View.inflate(context, R.layout.dialog_time, null);
+        TimePicker timePicker = (TimePicker) dialogView2.findViewById(R.id.timePicker);
+        timePicker.setCurrentHour(hour);
+        timePicker.setCurrentMinute(minute);
+        timePicker.setIs24HourView(true); //设置24小时制
+        timePicker.setOnTimeChangedListener(this);
+        dialog2.setTitle("设置时间");
+        dialog2.setView(dialogView2);
+        dialog2.show();
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (date.length() > 0) { //清除上次记录的日期
+                    date.delete(0, date.length());
+                }
+                dateInstuction.setText(date.append(String.valueOf(year)).append("年").append(String.valueOf(month)).append("月").append(day).append("日"));
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        View dialogView = View.inflate(context, R.layout.dialog_date, null);
+        final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
+
+        dialog.setTitle("设置日期");
+        dialog.setView(dialogView);
+        dialog.show();
+        //初始化日期监听事件
+        datePicker.init(year, month - 1, day, this);
+    }
+
+    /**
+     * 日期改变的监听事件
+     *
+     * @param view
+     * @param year
+     * @param monthOfYear
+     * @param dayOfMonth
+     */
+    @Override
+    public void onDateChanged(DatePicker view,int year,int monthOfYear,int dayOfMonth){
+        this.year=year;
+        this.month=monthOfYear;
+        this.day=dayOfMonth;
+    }
+
+    /**
+     * 时间改变的监听事件
+     *
+     * @param view
+     * @param hourOfDay
+     * @param minute
+     */
+    @Override
+    public void onTimeChanged(TimePicker view,int hourOfDay,int minute){
+        this.hour=hourOfDay;
+        this.minute=minute;
     }
 }
